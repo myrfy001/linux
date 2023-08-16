104745c7f284e24f049bf04922821e312577ef24
myrfy001 <11474381+myrfy001@users.noreply.github.com>
2023-08-16 13:25:24 +0800

    add dev and port attrs

--- drivers/infiniband/hw/dtld/dtld_verbs.h
+++ drivers/infiniband/hw/dtld/dtld_verbs.h
@@ -3,9 +3,21 @@
 
 #include <rdma/ib_verbs.h>
 
+
+struct dtld_port {
+	struct ib_port_attr	attr;
+	__be64			port_guid;
+	__be64			subnet_prefix;
+	spinlock_t		port_lock; /* guard port */
+	unsigned int		mtu_cap;
+	/* special QPs */
+	u32			qp_gsi_index;
+};
+
 struct dtld_dev {
 	struct ib_device	ib_dev;
 	struct ib_device_attr	attr;
+	struct dtld_port		port;
 };
 
 static inline struct dtld_dev *dtld_from_ibdev(struct ib_device *dev)
